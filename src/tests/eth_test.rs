13e11eb68c5eedc9735482f3c54ffc35c82a9793
Steve Kok <kokchoquan@gmail.com>
2025-07-04 00:08:42 +0800

    refactor: split out non-ethereum test case and general reusable code from the eth_test.rs

--- src/tests/eth_test.rs
+++ src/tests/eth_test.rs
@@ -1,39 +1,11 @@
-
-use near_sdk::test_utils::VMContextBuilder;
 use near_sdk::{testing_env, AccountId};
 
 use crate::SmartAccountContract;
 
-
-fn get_context() -> VMContextBuilder {
-    let mut builder = VMContextBuilder::new();
-    let account_id = AccountId::try_from("alice.near".to_string()).unwrap();
-    builder.predecessor_account_id(account_id);
-    builder
-}
-
-#[test]
-#[should_panic(expected = "E001: unsupported blockchain")]
-fn test_unsupported_blockchain() {
-    // 1. set up environment
-    let mut builder = get_context();
-    let caller = AccountId::try_from("rektdegen.testnet".to_string()).unwrap();
-    builder.current_account_id(caller.clone());
-    let context = builder.build();
-    testing_env!(context);
-    let mut contract = SmartAccountContract::default();
-
-    // 2. test signature verification success
-    let eth_blockchain = "random_chain".to_string();
-    let eth_address = "0x950918fe5deb16c90a7071d5f3daff3f2e84e0df".to_string();
-    let signature = "0xfce0288ef179953be8cf0d1eb10b2c8f008c591f89deb3425348e6f520cee12e4392c85ee9e81973af97999b84e88886fe12969c39bd663a2cf20725b8deb2a61c".to_string();
-    contract.add_recovery_address(eth_blockchain.clone(), eth_address, signature, 1);
-}
-
 #[test]
 fn test_add_recovery_address() {
     // 1. set up environment
-    let mut builder = get_context();
+    let mut builder = super::get_context();
     let caller = AccountId::try_from("rektdegen.testnet".to_string()).unwrap();
     builder.current_account_id(caller.clone());
     let context = builder.build();
@@ -53,7 +25,7 @@ fn test_add_recovery_address() {
 #[test]
 fn test_add_recovery_address_non_private_call() {
     // 1. set up environment
-    let mut builder = get_context();
+    let mut builder = super::get_context();
     let contract = AccountId::try_from("rektdegen.testnet".to_string()).unwrap();
     builder.current_account_id(contract.clone());
     let caller = AccountId::try_from("notrektdegen.testnet".to_string()).unwrap();
@@ -77,7 +49,7 @@ fn test_add_recovery_address_non_private_call() {
 #[should_panic(expected = "E004: signature verification failed")]
 fn test_add_recovery_address_invalid_signature() {
     // 1. set up environment
-    let mut builder = get_context();
+    let mut builder = super::get_context();
     let caller = AccountId::try_from("rektdegen.testnet".to_string()).unwrap();
     builder.current_account_id(caller.clone());
     let context = builder.build();
@@ -95,7 +67,7 @@ fn test_add_recovery_address_invalid_signature() {
 #[test]
 fn test_recover_success() {
     // 1. set up environment
-    let mut builder = get_context();
+    let mut builder = super::get_context();
     let caller = AccountId::try_from("rektdegen.testnet".to_string()).unwrap();
     builder.current_account_id(caller.clone());
     let context = builder.build();
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,1 +1,36 @@
+use near_sdk::test_utils::VMContextBuilder;
+use near_sdk::{testing_env, AccountId};
 mod eth_test;
+mod solana_test;
+
+use crate::SmartAccountContract;
+
+fn get_context() -> VMContextBuilder {
+    let mut builder = VMContextBuilder::new();
+    let account_id = AccountId::try_from("alice.near".to_string()).unwrap();
+    builder.predecessor_account_id(account_id);
+    builder
+}
+
+#[test]
+#[should_panic(expected = "E001: unsupported blockchain")]
+fn test_unsupported_blockchain() {
+    // 1. set up environment
+    let mut builder = get_context();
+    let caller = AccountId::try_from("rektdegen.testnet".to_string()).unwrap();
+    builder.current_account_id(caller.clone());
+    let context = builder.build();
+    testing_env!(context);
+    let mut contract = SmartAccountContract::default();
+
+    // 2. test signature verification success
+    let random_blockchain = "random_chain".to_string();
+    let random_address = "0x950918fe5deb16c90a7071d5f3daff3f2e84e0df".to_string();
+    let random_signature = "0xfce0288ef179953be8cf0d1eb10b2c8f008c591f89deb3425348e6f520cee12e4392c85ee9e81973af97999b84e88886fe12969c39bd663a2cf20725b8deb2a61c".to_string();
+    contract.add_recovery_address(
+        random_blockchain.clone(),
+        random_address,
+        random_signature,
+        1,
+    );
+}
