776ae597ff01277edd8628feadc6ad9eb2756c14
Steve Kok <kokchoquan@gmail.com>
2026-06-16 00:42:43 +0800

    test: allow slower testnet e2e runs

--- e2e-test/playwright.config.js
+++ e2e-test/playwright.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
-    timeout: 60_000,
+    timeout: 120_000,
     reporter: [['list']],
     use: {
         trace: 'on-first-retry',
